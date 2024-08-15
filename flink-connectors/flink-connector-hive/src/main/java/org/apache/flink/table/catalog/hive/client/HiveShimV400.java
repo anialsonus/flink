f439792c771c56a00ec087d8c26595fe4950a9e1
Andrey Shitov <sai@arenadata.io>
2024-08-15 13:01:14 +0300

    Apache hive 4.0.0 support.

--- flink-connectors/flink-connector-hive/src/main/java/org/apache/flink/table/catalog/hive/client/HiveShimV400.java
+++ flink-connectors/flink-connector-hive/src/main/java/org/apache/flink/table/catalog/hive/client/HiveShimV400.java
@@ -20,14 +20,20 @@ package org.apache.flink.table.catalog.hive.client;
 
 import org.apache.flink.table.catalog.exceptions.CatalogException;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hive.metastore.api.FieldSchema;
+import org.apache.hadoop.hive.metastore.api.Table;
 import org.apache.hadoop.hive.ql.exec.FileSinkOperator;
 import org.apache.hadoop.hive.ql.io.HiveOutputFormat;
 import org.apache.hadoop.hive.ql.io.HivePassThroughOutputFormat;
+import org.apache.hadoop.hive.serde2.Deserializer;
 import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.Reporter;
 
+import java.lang.reflect.Method;
+import java.util.List;
 import java.util.Properties;
 
 /** Shim for Hive version 4.0.0. */
@@ -50,6 +56,33 @@ public class HiveShimV400 extends HiveShimV313 {
                     jobConf, outPath, outValClz, isCompressed, tableProps, Reporter.NULL);
         } catch (Exception e) {
             throw new CatalogException("Failed to create Hive RecordWriter", e);
+        }
+    }
+
+    public List<FieldSchema> getFieldsFromDeserializer(
+            Configuration conf, Table table, boolean skipConfError) {
+        try {
+            Method utilMethod =
+                    getHiveMetaStoreUtilsClass()
+                            .getMethod(
+                                    "getDeserializer",
+                                    Configuration.class,
+                                    Table.class,
+                                    String.class,
+                                    boolean.class);
+            Deserializer deserializer =
+                    (Deserializer) utilMethod.invoke(null, conf, table, null, skipConfError);
+            utilMethod =
+                    getHiveMetaStoreUtilsClass()
+                            .getMethod(
+                                    "getFieldsFromDeserializer",
+                                    String.class,
+                                    Deserializer.class,
+                                    Configuration.class);
+            return (List<FieldSchema>)
+                    utilMethod.invoke(null, table.getTableName(), deserializer, conf);
+        } catch (Exception e) {
+            throw new CatalogException("Failed to get table schema from deserializer", e);
         }
     }
 }
